b5370ba8b958219e96ce9c9c0cac5aa666dd4283
Anton <hulkofsky@gmail.com>
2018-06-08 13:42:16 +0300

    done

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './main.sass'
+
+const Index = () => {
+  return <div>Hello React!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById('root'));
