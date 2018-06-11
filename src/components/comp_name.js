b43a0ef3a277a4f56f65fd93d0352680e9b05d7d
Anton <hulkofsky@gmail.com>
2018-06-11 09:52:40 +0300

    done

---
+++ src/components/comp_name.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const WebPage = () => (
+    <Grid>
+        
+    </Grid>
+);
+
+export default WebPage;
---
+++ src/containers/cont_name.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import {bindActionCreators} from 'redux';
+import {connect} from 'react-redux';
+import {} from '../actions/actions';
+
+class ContactList extends Component {
+
+    showlist(searchCriteria){
+        console.log(searchCriteria, 'in showlist');
+        return this.props.contacts.map((contact) => {
+            return (
+                <List.Item onClick={() => this.props.select(contact)} key={contact.id}>
+                    <Image avatar src={contact.photo}/>
+                    <List.Content>
+                        <List.Header>{contact.name}</List.Header>
+                        {contact.company}
+                    </List.Content>
+                </List.Item>
+            );
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <Input icon='search' iconPosition='left' placeholder='Search users...' onChange={() => {
+                            const searchCriteria = this.props.search(document.querySelector('input[type=text]').value).payload;
+                            
+                            return (
+                                <List animated celled verticalAlign='middle'>
+                                    {this.showlist(searchCriteria)}
+                                </List>
+                            );
+                        }
+                    }/>
+            </div>
+        );
+    }
+}
+
+function mapStateToProps (state) {
+    return {
+        contacts: state.contacts
+    };
+}
+
+function matchDispatchToProps (dispatch) {
+    return bindActionCreators({select: select, search: search}, dispatch);
+}
+
+export default connect(mapStateToProps, matchDispatchToProps)(ContactList);
